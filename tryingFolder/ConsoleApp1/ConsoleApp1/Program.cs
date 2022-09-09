e73d001d4ef65ae73282cbea7319ce6f3f573ea6
Vadim <vadimzharsky1990@gmail.com>
2022-09-09 13:13:46 +0300

    0.2

--- tryingFolder/ConsoleApp1/ConsoleApp1/Program.cs
+++ tryingFolder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,61 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> evens = new List<int>();
+            List<int> notEvens = new List<int>();
 
 
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+            for (int i = 0; i < 100; i++)
+            {
+                int a = rnd.Next(1, 10);
+                if (decomposer(a) == 0)
+                {
+                    evens.Add(a);
+                }
+                else
+                {
+                    notEvens.Add(a);
+                }
+            }
+            int evenSum = summator("evens", evens);
+            int notEvenSum = summator("not evens", notEvens);
+            Console.WriteLine();
+            Console.WriteLine($"Sum in \"Evens\" list is : {evenSum}");
+            Console.WriteLine($"Sum in \"Not Evens\" list is : {notEvenSum}");
+
+            Console.ReadKey();
+
+        }
+
+        public static int decomposer(int a)
+        {
+            if (a % 2 == 0)
+                return 0; 
+            else
+                return 1;
+        }
+        public static int summator(string name, List<int>lst)
+        {
+            Console.WriteLine($"...Numbers in {name} list");
+            int sum = 0;
+            foreach (int i in lst)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
